e8b545f12994a2da6da0e3939e2015f99f0457b2
binipk <mingyoku93@gmail.com>
2025-06-12 20:40:28 +0900

    all files are updated

---
+++ room-service/src/main/java/com/unobnb/roomservice/controller/RoomController.java
@@ -0,0 +1,49 @@
+package com.unobnb.roomservice.controller;
+
+import com.unobnb.roomservice.dto.RoomDTO;
+import com.unobnb.roomservice.service.RoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/room-service")
+@RequiredArgsConstructor
+public class RoomController {
+
+    private final RoomService roomService;
+
+    @PostMapping
+    public ResponseEntity<RoomDTO> createRoom(@RequestBody RoomDTO roomDTO) {
+        RoomDTO saveRoom = roomService.save(roomDTO);
+        return ResponseEntity.ok(saveRoom);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<RoomDTO>> getAllRooms() {
+        return ResponseEntity.ok(roomService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RoomDTO> getRoomById(@PathVariable Long id) {
+        return ResponseEntity.ok(roomService.findById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<RoomDTO> updateRoom(
+            @PathVariable Long id,
+            @RequestBody RoomDTO roomDTO
+    ) {
+        roomDTO.setId(id);
+        return ResponseEntity.ok(roomService.update(roomDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteRoom(@PathVariable Long id) {
+        roomService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
