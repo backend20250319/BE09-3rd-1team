ae6cb6576349b38dc2fe05e7fac10a54d0306364
gyongcode <gyongcode@gmail.com>
2025-06-12 12:35:38 +0900

    add eureka-server

---
+++ dicovery-service/src/main/java/com/unobnb/dicoveryservice/DicoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package com.unobnb.dicoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DicoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DicoveryServiceApplication.class, args);
+    }
+
+}
