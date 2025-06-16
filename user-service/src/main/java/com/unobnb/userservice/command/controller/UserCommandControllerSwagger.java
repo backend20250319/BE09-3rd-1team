76d20e7456baf0482f8e2a077107021844e5085c
gyongcode <gyongcode@gmail.com>
2025-06-16 10:48:06 +0900

    수정

--- user-service/src/main/java/com/unobnb/userservice/command/controller/UserCommandControllerSwagger.java
+++ user-service/src/main/java/com/unobnb/userservice/command/controller/UserCommandControllerSwagger.java
@@ -9,8 +9,8 @@ import org.springframework.http.ResponseEntity;
 @Tag(name = "User API")
 public interface UserCommandControllerSwagger {
 
-    @Operation(summary = "자신의 회원 정보 조회", description = "아이디, 비밀번호를 ")
-    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "회원정보 조회 성공")
+    @Operation(summary = "회원 가입", description = "아이디, 비밀번호가 있어야함")
+    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "회원가입 성공")
     public ResponseEntity<ApiResponse<Void>> register(UserCreateRequestDTO requestDto);
 
 }
