073593abba5008564802601e75c4465323c2f782
junga970 <junga2940@gmail.com>
2025-06-16 09:20:17 +0900

    예외처리

--- reservation-service/src/main/java/com/ohgiraffers/reservationservice/command/client/RoomClient.java
+++ reservation-service/src/main/java/com/ohgiraffers/reservationservice/command/client/RoomClient.java
@@ -1,23 +1,26 @@
 package com.ohgiraffers.reservationservice.command.client;
 
 import com.ohgiraffers.reservationservice.command.dto.RoomDTO;
-import com.ohgiraffers.reservationservice.command.dto.RoomIdListReqDTO;
-import com.ohgiraffers.reservationservice.common.ApiResponse;
+import com.ohgiraffers.reservationservice.command.dto.RoomListRequest;
 import com.ohgiraffers.reservationservice.config.FeignClientConfig;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-// 내부에서 user-service를 호출하는 상황
+// 내부에서 room-service를 호출하는 상황
 @FeignClient(name = "room-service", configuration = FeignClientConfig.class)
 public interface RoomClient {
+    // 내부에서 room-service를 호출하는 상황
+    @GetMapping("/rooms/{id}")
+    RoomDTO getRoomById(@PathVariable("id") Long id);
 
     // 내부에서 user-service를 호출하는 상황
-    @PostMapping("/api/v1/room-service/list")
-    List<RoomDTO> getRoomsByIds(@RequestBody RoomIdListReqDTO request);
+    @PostMapping("/rooms/list")
+    List<RoomDTO> getRoomsByIds(@RequestBody RoomListRequest request);
 }
 
 
